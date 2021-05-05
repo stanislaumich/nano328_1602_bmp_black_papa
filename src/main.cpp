b372f9adb04ede5f87015e95acf48eb3e5fc5b80
stanislaumich <staslz42stas@gmail.com>
2021-05-05 11:46:46 +0300

    RUS OK

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+//-------БИБЛИОТЕКИ---------
+#include "LowPower.h"
+#include "EEPROMex.h"
+#include "LCD_1602_RUS.h"
+//-------БИБЛИОТЕКИ---------
+
+LCD_1602_RUS lcd(0x27, 16, 2);            // создать дисплей
+// если дисплей не работает, замени 0x27 на 0x3f
+
+void setup() {
+  Serial.begin(9600); 
+   lcd.init();
+  lcd.backlight();
+  lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(L"Привет");
+
+
+}
+void loop(){
+
+  
+}
+
