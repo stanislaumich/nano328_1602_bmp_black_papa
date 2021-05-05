76d258b98e47d23fd0c49dfcbd026e138dc9cfe4
stanislaumich <staslz42stas@gmail.com>
2021-05-05 15:29:28 +0300

    long-short

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 LCD_1602_RUS lcd(0x27, 16, 2);            // создать дисплей
 // если дисплей не работает, замени 0x27 на 0x3f
 
+uint32_t ms_button = 0;
+bool     button_state = false;
+bool     button_long_state = false;
+
 void setup() {
   Serial.begin(9600); 
    lcd.init();
@@ -15,21 +19,36 @@ void setup() {
   lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print(L"Привет, ромашки");
-
-
 }
+
 void loop(){
+
+ uint32_t ms    = millis();
+
+
  int i=analogRead(A3);
- lcd.setCursor(0, 1);
- if (i>1022){
- lcd.print("OFF");
- }else{
-  lcd.print("ON "); 
- }
- delay(10);
+   if( i<1022 && !button_state && ( ms - ms_button ) > 50 ){
+      button_state      = true;
+      button_long_state = false;  
+      ms_button         = ms;
+      lcd.setCursor(0, 1);
+      lcd.print("    ");
+   }
+// Фиксируем длинное нажатие кнопки   
+   if( i<1022 && !button_long_state && ( ms - ms_button ) > 2000 ){
+      button_long_state = true;
+      Serial.println("Long press key");
+      lcd.setCursor(0, 1);
+      lcd.print("LONG");    
+   }
+// Фиксируем отпускание кнопки   
+   if( i>1022 && button_state && ( ms - ms_button ) > 50  ){
+      button_state      = false;   
+      ms_button         = ms;
+      if( !button_long_state ){
+        lcd.setCursor(0, 1);
+        lcd.print("SHOR");   
+      }
+   }
+
 }
-
-/*
-
-
-*/
